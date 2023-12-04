e3d8395e237f485cc56fe2646dd30f84141b777d
Bram Borggreve <beeman@beeman.nl>
2023-12-04 02:21:04 +0000

    feat: add wallet adapters with common mobile apps

--- demo/src/app/home/feature/home-feature.tsx
+++ demo/src/app/home/feature/home-feature.tsx
@@ -1,6 +1,9 @@
 import { Container, Stack } from '@mantine/core'
-
 import { WalletModalProvider } from '@pubkeyapp/wallet-adapter-mantine-ui'
+import { BackpackWalletAdapter } from '@solana/wallet-adapter-backpack'
+import { GlowWalletAdapter } from '@solana/wallet-adapter-glow'
+import { PhantomWalletAdapter } from '@solana/wallet-adapter-phantom'
+import { SolflareWalletAdapter } from '@solana/wallet-adapter-solflare'
 import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react'
 import { clusterApiUrl } from '@solana/web3.js'
 import { HomeUiExample, HomeUiHero } from '../ui'
@@ -9,7 +12,14 @@ export function HomeFeature() {
   return (
     <Container size="xl">
       <ConnectionProvider endpoint={clusterApiUrl('devnet')}>
-        <WalletProvider wallets={[]}>
+        <WalletProvider
+          wallets={[
+            new BackpackWalletAdapter(),
+            new GlowWalletAdapter(),
+            new PhantomWalletAdapter(),
+            new SolflareWalletAdapter(),
+          ]}
+        >
           <WalletModalProvider>
             <Stack gap="xl">
               <HomeUiHero />
