3eb88cc30b1474a78f296762dc81b29e58ed32f0
Bram Borggreve <beeman@beeman.nl>
2023-12-20 06:25:21 +0000

    demo: add more component variants

--- demo/src/app/features/components/components-ui-overview.tsx
+++ demo/src/app/features/components/components-ui-overview.tsx
@@ -1,5 +1,5 @@
-import { Group, SimpleGrid } from '@mantine/core'
-import { UiStack } from '@pubkey-ui/core'
+import { Button, ButtonProps, Divider, Group, SimpleGrid } from '@mantine/core'
+import { UiDebug, UiGroup, UiStack } from '@pubkey-ui/core'
 import {
   WalletDisconnectButton,
   WalletModalButton,
@@ -7,10 +7,12 @@ import {
   WalletMultiIcon,
 } from '@pubkeyapp/wallet-adapter-mantine-ui'
 import { AppDemo } from '../../ui'
+import { useEffect, useState } from 'react'
 
 export function ComponentsUiOverview() {
   return (
     <UiStack>
+      <Divider label="Buttons" />
       <SimpleGrid cols={{ base: 1, md: 2 }}>
         <AppDemo title="WalletMultiButton">
           <WalletMultiButton size="lg" />
@@ -25,24 +27,123 @@ export function ComponentsUiOverview() {
           <WalletMultiIcon size="lg" />
         </AppDemo>
       </SimpleGrid>
-      <AppDemo title="WalletMultiButton sizes">
-        <SimpleGrid cols={{ base: 1, md: 2 }}>
-          <WalletMultiButton size="xs" />
-          <WalletMultiButton size="sm" />
-          <WalletMultiButton size="md" />
-          <WalletMultiButton size="lg" />
-          <WalletMultiButton size="xl" />
-        </SimpleGrid>
-      </AppDemo>
-      <AppDemo title="WalletMultiIcon sizes">
-        <Group justify="center">
-          <WalletMultiIcon size="xs" />
-          <WalletMultiIcon size="sm" />
-          <WalletMultiIcon size="md" />
-          <WalletMultiIcon size="lg" />
-          <WalletMultiIcon size="xl" />
-        </Group>
-      </AppDemo>
+      <Divider label="Random" />
+      <DemoRandom />
+      <Divider label="Button variants" />
+      <DemoVariants />
+      <Divider label="Button sizes" />
+      <DemoSizes />
+      <Divider label="Button radius" />
+      <DemoRadius />
+      <Divider label="Button colors" />
+      <ButtonColors />
     </UiStack>
+  )
+}
+
+const sizes: ButtonProps['size'][] = ['xl', 'lg', 'md', 'sm', 'xs']
+const radii: ButtonProps['radius'][] = ['xs', 'sm', 'md', 'lg', 'xl']
+const variants: ButtonProps['variant'][] = ['default', 'filled', 'light', 'outline', 'subtle', 'transparent', 'white']
+const colors: ButtonProps['color'][] = [
+  'red',
+  'pink',
+  'grape',
+  'violet',
+  'indigo',
+  'blue',
+  'cyan',
+  'teal',
+  'green',
+  'lime',
+  'yellow',
+  'orange',
+  'gray',
+]
+
+function DemoRandom() {
+  const [size, setSize] = useState<ButtonProps['size']>('md')
+  const [radius, setRadius] = useState<ButtonProps['radius']>('md')
+  const [variant, setVariant] = useState<ButtonProps['variant']>('default')
+  const [color, setColor] = useState<ButtonProps['color']>('blue')
+
+  function randomize() {
+    setSize(sizes[Math.floor(Math.random() * sizes.length)])
+    setRadius(radii[Math.floor(Math.random() * radii.length)])
+    setVariant(variants[Math.floor(Math.random() * variants.length)])
+    setColor(colors[Math.floor(Math.random() * colors.length)])
+  }
+  useEffect(() => {
+    randomize()
+  }, [])
+
+  return (
+    <UiGroup align="start" justify="space-around">
+      <Button onClick={randomize}>Randomize</Button>
+      <Group justify="center">
+        <WalletMultiButton size={size} radius={radius} variant={variant} color={color} />
+        <WalletMultiIcon size={size} radius={radius} variant={variant} color={color} />
+      </Group>
+      <UiDebug data={{ size, radius, variant, color }} open hideButton />
+    </UiGroup>
+  )
+}
+
+function DemoSizes() {
+  return (
+    <SimpleGrid cols={{ base: 1, sm: 2, lg: 5 }}>
+      {sizes.map((size) => (
+        <AppDemo key={size} title={`size="${size}"`}>
+          <Group>
+            <WalletMultiButton size={size} />
+            <WalletMultiIcon size={size} />
+          </Group>
+        </AppDemo>
+      ))}
+    </SimpleGrid>
+  )
+}
+
+function DemoRadius() {
+  return (
+    <SimpleGrid cols={{ base: 1, sm: 2, lg: 5 }}>
+      {radii.map((radius) => (
+        <AppDemo key={radius} title={`radius="${radius}"`}>
+          <Group>
+            <WalletMultiButton radius={radius} />
+            <WalletMultiIcon radius={radius} />
+          </Group>
+        </AppDemo>
+      ))}
+    </SimpleGrid>
+  )
+}
+
+function DemoVariants() {
+  return (
+    <SimpleGrid cols={{ base: 1, sm: 2, lg: 4 }}>
+      {variants.map((variant) => (
+        <AppDemo key={variant} title={`variant="${variant}"`}>
+          <Group>
+            <WalletMultiButton variant={variant} />
+            <WalletMultiIcon variant={variant} />
+          </Group>
+        </AppDemo>
+      ))}
+    </SimpleGrid>
+  )
+}
+
+function ButtonColors() {
+  return (
+    <SimpleGrid cols={{ base: 1, sm: 2, lg: 4 }}>
+      {colors.map((color) => (
+        <AppDemo key={color} title={`color="${color}"`}>
+          <Group>
+            <WalletMultiButton color={color} />
+            <WalletMultiIcon color={color} />
+          </Group>
+        </AppDemo>
+      ))}
+    </SimpleGrid>
   )
 }
